62cecbe4873495af9652b56e9173644019f8068c
thesimmons <simao.moreno@widgilabs.com>
2023-05-31 17:14:40 +0100

    Clear existing code

--- src/App.java
+++ src/App.java
@@ -1,32 +1,5 @@
-import model.PostoTrabalho;
-import model.Transportadora;
-
 public class App {
     public static void main(String[] args) throws Exception {
-
-        // Criando os objetos de postos de trabalho
-        PostoTrabalho postoA = new PostoTrabalho("A", 1, 12, 50);
-        PostoTrabalho postoB = new PostoTrabalho("B", 2, 0, 100);
-        PostoTrabalho postoC = new PostoTrabalho("C", 1, 14, 100);
-        PostoTrabalho postoD = new PostoTrabalho("D", 2, 0, 100);
-
-        // Criando os objetos de transportadoras
-        Transportadora t1 = new Transportadora("T1", 1);
-        Transportadora t2 = new Transportadora("T2", 1);
-
-        // Estabelecendo as associações entre postos de trabalho e transportadoras
-        postoA.setSaida(t1);
-        postoB.setEntrada(t1);
-        postoB.setSaida(t2);
-        postoC.setEntrada(t1);
-        postoC.setSaida(t2);
-        postoD.setEntrada(t2);
-        postoD.setSaida(null); // Saída para o depósito de produtos
-
-        t1.setEntrada(postoA);
-        t1.setSaida(postoB);
-        t2.setEntrada(postoB);
-        t2.setSaida(postoD);
 
         System.out.println("Hello, World!");
     }
--- src/model/Transportadora.java
+++ src/model/Transportadora.java
@@ -1,53 +1,5 @@
 package model;
 
 public class Transportadora {
-    private String id;
-    private int capacidade;
-    private PostoTrabalho entrada;
-    private PostoTrabalho saida;
-    // outros atributos necessários
-
-    public Transportadora(String id, int capacidade) {
-        this.id = id;
-        this.capacidade = capacidade;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public int getCapacidade() {
-        return capacidade;
-    }
-
-    public boolean temCapacidadeDisponivel() {
-        // lógica para verificar se a transportadora tem capacidade disponível
-    }
-
-    public PostoTrabalho getEntrada() {
-        return entrada;
-    }
-
-    public PostoTrabalho getSaida() {
-        return saida;
-    }
-
-    public void setEntrada(PostoTrabalho entrada) {
-        this.entrada = entrada;
-    }
-
-    public void setSaida(PostoTrabalho saida) {
-        this.saida = saida;
-    }
-
-    public void adicionarProduto() {
-        // lógica para adicionar um produto à transportadora
-    }
-
-    public void removerProduto() {
-        // lógica para remover um produto da transportadora
-    }
-
-    // outros métodos relevantes
 }
 
